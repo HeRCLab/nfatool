128b047d8754a226985c8e6f1d9c6658deec2c32
Rasha Karakchi <rasha.karakchi@gmail.com>
2018-03-20 20:58:24 -0400

    add id to dump_to_dot

--- visualization.cpp
+++ visualization.cpp
@@ -27,6 +27,24 @@ void dump_dot_file (char *filename, xmlNode *aNode, vector<int> subset) {
         i++;
       }
   }
+
+
+int *id; 
+	
+//static char str[1024], *str2=str+2; 
+xmlAttr *attr; 
+int k=0; 
+   for(aNode = aNode->children; aNode; aNode = aNode->next) {
+		printf("k=%d\n", k++) ; 
+         for (attr = aNode->properties; attr; attr = attr->next){
+                if(!strcmp((const char *)attr->name,"id")) { 
+			*id =state_map[(const char *)attr->children->content];
+			fprintf (myFile, "%d\n", *id); 
+		}
+	}
+   }
+
+
   for (int i=0;i<num_states;i++) {
     for (int j=0;(j<max_edges) && (edge_table[i][j]!=-1);j++) {
         found=0;
@@ -38,6 +56,9 @@ void dump_dot_file (char *filename, xmlNode *aNode, vector<int> subset) {
         if (found && found2) fprintf (myFile,"%d -> %d;\n",i,edge_table[i][j]);
     }
   }
+
+ 
+
   fprintf(myFile,"}\n");
   fclose(myFile);
 }
